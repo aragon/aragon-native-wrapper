fa3aac585be2afbc3dec103eb8d16a3c5e300427
Jorge Izquierdo <jorge@izqui.me>
2017-01-11 18:39:36 +0100

    Class to object

--- keybase/index.js
+++ keybase/index.js
@@ -4,26 +4,26 @@ const KeybaseRPC = require('./rpc')
 
 const rpc = new KeybaseRPC()
 
-const proofFilename = 'ethereum_test.json' // TODO: Some day it won't be a test lol
 const keybasePublicDirectory = user => path.join('/keybase/public', user || String())
-const proofPath = user => path.join(keybasePublicDirectory(user), proofFilename)
+const proofPath = (user, filename) => path.join(keybasePublicDirectory(user), filename)
 
 // TODO: Promisify all this?
-const Keybase = {
-  getUsername: (cb) => {
-    rpc.invoke('config.getCurrentStatus', null, (err, res) => {
-      console.log(res)
-      if (!res.loggedIn) return cb(false)
+const getUsername = (cb) => {
+  rpc.invoke('config.getCurrentStatus', null, (err, res) => {
+    console.log(res)
+    if (!res.loggedIn) return cb(false)
 
-      return cb(null, res.user.username)
-    })
-  },
-  saveProof: (proofPayload, cb) => {
-    console.log(proofPayload)
-    this.getUsername((err, username) => {
+    return cb(null, res.user.username)
+  })
+}
+
+const Keybase = {
+  getUsername,
+  saveProof: (proofPayload, filename, cb) => {
+    getUsername((err, username) => {
       const proof = JSON.parse(proofPayload)
       if (username !== proof.username) { return cb(new Error('username doesnt match')) }
-      return fs.writeFile(proofPath(username), proofPayload, cb)
+      return fs.writeFile(proofPath(username, filename), proofPayload, cb)
     })
   },
 }
